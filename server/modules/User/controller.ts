0a91caaa2cc156c743e053ac3fb3ad514eba7d25
diegodifreitas <diegodifreitas@gmail.com>
2017-08-27 01:08:56 -0300

    Integration controller with fn partial of lodash

--- server/modules/User/controller.ts
+++ server/modules/User/controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from 'express';
 import * as HTTPStatus from 'http-status';
+import * as _ from 'lodash';
+
+import { onError } from '../../api/responses/errorHandler';
+import { onSucess } from '../../api/responses/sucessHandler';
+import { dbErrorHandler } from '../../config/dbErrorHandler';
 
 import User from './service';
 
@@ -13,76 +18,37 @@ class UserController {
 
   public getAll(req: Request, res: Response) {
     this.UserService.getAll()
-      .then((data) => {
-        res.status(HTTPStatus.OK).json({
-          payload: data,
-        });
-      })
-      .catch((error) => {
-        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
-          payload: 'Erro ao buscar todos os usuários',
-        });
-      });
+      .then(_.partial(onSucess, res))
+      .catch(_.partial(onError, res, 'Erro ao buscar todos os usuários'));
   }
 
   public createUser(req: Request, res: Response) {
     this.UserService.create(req.body)
-      .then((data) => {
-        res.status(HTTPStatus.OK).json({
-          payload: data,
-        });
-      })
-      .catch((error) => {
-        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
-          payload: 'Erro ao cadastrar novo usuário',
-        });
-      });
+      .then(_.partial(onSucess, res))
+      .catch(_.partial(dbErrorHandler, res))
+      .catch(_.partial(onError, res, 'Erro ao inserir novo usuário'));
   }
 
   public getById(req: Request, res: Response) {
     const userId = parseInt(req.params.id, 10);
     this.UserService.getById(userId)
-      .then((data) => {
-        res.status(HTTPStatus.OK).json({
-          payload: data,
-        });
-      })
-      .catch((error) => {
-        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
-          payload: 'Erro ao buscar usuário',
-        });
-      });
+      .then(_.partial(onSucess, res))
+      .catch(_.partial(onError, res, 'Usuario não encontrado'));
   }
 
   public updateUser(req: Request, res: Response) {
     const userId = parseInt(req.params.id, 10);
     const props = req.body;
     this.UserService.update(userId, props)
-      .then((data) => {
-        res.status(HTTPStatus.OK).json({
-          payload: data,
-        });
-      })
-      .catch((error) => {
-        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
-          payload: 'Erro ao atualizar usuário',
-        });
-      });
+      .then(_.partial(onSucess, res))
+      .catch(_.partial(onError, res, 'Erro ao atualizar usuário'));
   }
 
   public deleteUser(req: Request, res: Response) {
     const userId = parseInt(req.params.id, 10);
     this.UserService.delete(userId)
-      .then((data) => {
-        res.status(HTTPStatus.OK).json({
-          payload: data,
-        });
-      })
-      .catch((error) => {
-        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
-          payload: 'Erro ao deletar usuário',
-        });
-      });
+      .then(_.partial(onSucess, res))
+      .catch(_.partial(onError, res, 'Erro ao excluir usuário'));
   }
 }
 
