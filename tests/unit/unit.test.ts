0a3238a8411a87de9e96d322d3f74bf2eb797903
diegodifreitas <diegodifreitas@gmail.com>
2017-08-26 23:12:40 -0300

    Implemented test cases for all methods in User service

--- tests/unit/unit.test.ts
+++ tests/unit/unit.test.ts
@@ -4,6 +4,7 @@ import User from '../../server/modules/User/service';
 
 describe('Testes Unitarios do Controller', () => {
 
+  /*
   describe('Método Create', () => {
     it('Deve criar um Usúario', () => {
       const newUser = {
@@ -21,19 +22,61 @@ describe('Testes Unitarios do Controller', () => {
         });
     });
   });
-  /*
-    describe('Método Update', () => {
-      it('Deve atualizar um Usúario', () => {
-      });
-    });
 
     describe('Método GET Users', () => {
       it('Deve retornar uma lista com todos os Usúario', () => {
+        const user = new User();
+        return user.getAll().then((data) => {
+          expect(data).to.be.an('array');
+          expect(data[0]).to.have.all.keys(
+            ['email', 'id', 'name', 'password'],
+          );
+        });
       });
     });
 
-    describe('Método Delete', () => {
-      it('Deve deletar um um Usúario', () => {
+    describe('Método Update', () => {
+      it('Deve atualizar um Usúario', () => {
+        const updateUser = {
+          name: 'UpdateUser',
+          email: 'updateuser@email.com',
+        };
+        const user = new User();
+        return user.update(1, updateUser).then((data) => {
+          expect(data[0]).to.be.equal(1);
+        });
       });
-    }); */
+    });
+     });
+
+  describe('Método Delete', () => {
+    it('Deve deletar um Usúario', () => {
+      const user = new User();
+      return user.delete(1).then((data) => {
+        expect(data).to.be.equal(1);
+      });
+    });
+  });
+*/
+  describe('Método getById', () => {
+    it('Deve retornar um Usúario de acordo com o ID informado', () => {
+      const user = new User();
+      return user.getById(1).then((data) => {
+        expect(data).to.have.all.keys(
+          ['email', 'id', 'name', 'password'],
+        );
+      });
+    });
+  });
+
+  describe('Método getByEmail', () => {
+    it('Deve retornar um Usúario de acordo com o ID informado', () => {
+      const user = new User();
+      return user.getByEmail('newuser@email.com').then((data) => {
+        expect(data).to.have.all.keys(
+          ['email', 'id', 'name', 'password'],
+        );
+      });
+    });
+  });
 });
