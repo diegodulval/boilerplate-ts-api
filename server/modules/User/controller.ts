407ab785ddf89343c4a0cb19188beac8a31c0bd5
diegodifreitas <diegodifreitas@gmail.com>
2017-08-26 23:44:08 -0300

    Refactoring User controller

--- server/modules/User/controller.ts
+++ server/modules/User/controller.ts
@@ -1,37 +1,88 @@
 import { Request, Response } from 'express';
 import * as HTTPStatus from 'http-status';
 
+import User from './service';
+
 class UserController {
-  constructor() { }
+
+  public UserService: User;
+
+  constructor() {
+    this.UserService = new User();
+  }
 
   public getAll(req: Request, res: Response) {
-    res.status(HTTPStatus.OK).json({
-      message: 'OK',
-    });
+    this.UserService.getAll()
+      .then((data) => {
+        res.status(HTTPStatus.OK).json({
+          payload: data,
+        });
+      })
+      .catch((error) => {
+        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+          payload: 'Erro ao buscar todos os usuários',
+        });
+      });
   }
 
   public createUser(req: Request, res: Response) {
-    res.status(HTTPStatus.OK).json({
-      message: 'OK',
-    });
+    this.UserService.create(req.body)
+      .then((data) => {
+        res.status(HTTPStatus.OK).json({
+          payload: data,
+        });
+      })
+      .catch((error) => {
+        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+          payload: 'Erro ao cadastrar novo usuário',
+        });
+      });
   }
 
   public getById(req: Request, res: Response) {
-    res.status(HTTPStatus.OK).json({
-      message: 'OK',
-    });
+    const userId = parseInt(req.params.id, 10);
+    this.UserService.getById(userId)
+      .then((data) => {
+        res.status(HTTPStatus.OK).json({
+          payload: data,
+        });
+      })
+      .catch((error) => {
+        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+          payload: 'Erro ao buscar usuário',
+        });
+      });
   }
 
   public updateUser(req: Request, res: Response) {
-    res.status(HTTPStatus.OK).json({
-      message: 'OK',
-    });
+    const userId = parseInt(req.params.id, 10);
+    const props = req.body;
+    this.UserService.update(userId, props)
+      .then((data) => {
+        res.status(HTTPStatus.OK).json({
+          payload: data,
+        });
+      })
+      .catch((error) => {
+        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+          payload: 'Erro ao atualizar usuário',
+        });
+      });
   }
 
   public deleteUser(req: Request, res: Response) {
-    res.status(HTTPStatus.OK).json({
-      message: 'OK',
-    });
+    const userId = parseInt(req.params.id, 10);
+    this.UserService.delete(userId)
+      .then((data) => {
+        res.status(HTTPStatus.OK).json({
+          payload: data,
+        });
+      })
+      .catch((error) => {
+        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+          payload: 'Erro ao deletar usuário',
+        });
+      });
   }
 }
 
