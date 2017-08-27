e21f92c1d82ecae12c364ca0a205622e7a17d93e
diegodifreitas <diegodifreitas@gmail.com>
2017-08-26 22:42:29 -0300

    Implemented test case for create user

--- tests/unit/unit.test.ts
+++ tests/unit/unit.test.ts
@@ -1,24 +1,39 @@
 import { expect, testDouble } from './config/helpers';
 
+import User from '../../server/modules/User/service';
+
 describe('Testes Unitarios do Controller', () => {
 
   describe('Método Create', () => {
     it('Deve criar um Usúario', () => {
+      const newUser = {
+        id: 1,
+        name: 'NewUser',
+        email: 'newuser@email.com',
+        password: 'pinkfloyd',
+      };
+      const user = new User();
+      return user.create(newUser)
+        .then((data) => {
+          expect(data.dataValues).to.have.all.keys(
+            ['email', 'id', 'name', 'password', 'updatedAt', 'createdAt'],
+          );
+        });
     });
   });
+  /*
+    describe('Método Update', () => {
+      it('Deve atualizar um Usúario', () => {
+      });
+    });
 
-  describe('Método Update', () => {
-    it('Deve atualizar um Usúario', () => {
+    describe('Método GET Users', () => {
+      it('Deve retornar uma lista com todos os Usúario', () => {
+      });
     });
-  });
 
-  describe('Método GET Users', () => {
-    it('Deve retornar uma lista com todos os Usúario', () => {
-    });
-  });
-
-  describe('Método Delete', () => {
-    it('Deve deletar um um Usúario', () => {
-    });
-  });
+    describe('Método Delete', () => {
+      it('Deve deletar um um Usúario', () => {
+      });
+    }); */
 });
