4840cd11a96f836378c9651b4d8b8b461aafc44a
diegodifreitas <diegodifreitas@gmail.com>
2017-08-28 17:22:01 -0300

    Stabilizing environment

---
+++ server/config/env/production.env.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+module.exports = {
+  env: 'production',
+  db: 'moment',
+  dialect: 'postgres',
+  username: 'postgres',
+  password: 'postgres',
+  host: 'localhost',
+  serverPort: 3001,
+  pgPort: 5432,
+  dbURL: 'postgres://postgres:postgres@localhost:5432/moment',
+  secret: 'S3cr3t',
+};
