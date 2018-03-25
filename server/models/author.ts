5d1e70e1f6f968fad5cf36a1cfd1d6b36b9057c0
DiegoDulval <diegodifreitas@gmail.com>
2018-03-25 15:58:02 -0300

    Create model Author

---
+++ server/models/author.ts
@@ -0,0 +1,19 @@
+export default function (sequelize, DataTypes) {
+
+    const Author = sequelize.define('Author', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        }
+    });
+
+    return Author;
+}
