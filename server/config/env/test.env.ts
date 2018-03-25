7576479969cf5ff6a2f2a6be1fc9328f168f15a7
DiegoDulval <diegodifreitas@gmail.com>
2018-03-25 16:18:48 -0300

    Updated config bd

--- server/config/env/test.env.ts
+++ server/config/env/test.env.ts
@@ -1,13 +1,13 @@
 /* tslint:disable */
 module.exports = {
   env: 'development',
-  db: 'moment-test',
+  db: 'ts-api-test',
   dialect: 'postgres',
   username: 'postgres',
   password: 'postgres',
   host: 'localhost',
   serverPort: 3001,
   pgPort: 5432,
-  dbURL: 'postgres://postgres:postgres@localhost:5432/moment-test',
+  dbURL: 'postgres://postgres:postgres@localhost:5432/ts-api-test',
   secret: 'S3cr3t',
 };
